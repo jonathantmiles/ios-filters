4e01d21ab125be6e8ca63e2e3bcfa7fe89dc28ca
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-25 13:17:56 -0700

    includes blank project

---
+++ Filters/Filters/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Filters
+//
+//  Created by Jonathan T. Miles on 10/25/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Filters/Filters/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Filters
+//
+//  Created by Jonathan T. Miles on 10/25/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
